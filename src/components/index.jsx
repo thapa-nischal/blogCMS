765d42cc89efda4c71517fd650b00fa62dbb0645
Nischal Thapa <thapanischalji@gmail.com>
2024-07-18 00:39:53 +0545

    default component exports

---
+++ src/components/index.jsx
@@ -0,0 +1,4 @@
+export { default as Navbar } from "./Navbar";
+export { default as Footer } from "./Footer";
+export { default as Blogs } from "./Blogs";
+export { default as Editor } from "./Editor";
