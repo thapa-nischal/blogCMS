da291b47fff40763afab391b1701226e40a84173
Nischal Thapa <thapanischalji@gmail.com>
2024-09-15 02:28:36 +0545

    server url added

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: process.env.VITE_API_URL || "http://localhost:3000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
